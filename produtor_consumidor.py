f571cdda322d8844de1c511f66505f902bf6c7ea
tmmarquess <thiago.uchoa18@gmail.com>
2022-08-30 23:44:06 -0400

    Comentando o código do produtor/consumidor

--- produtor_consumidor.py
+++ produtor_consumidor.py
@@ -1,6 +1,5 @@
 import threading
 import random
-import time
 
 
 class bcolors:
@@ -10,23 +9,23 @@ class bcolors:
     RESET = "\033[0m"  # RESET COLOR
 
 
-buffer = []
+buffer = [] # Buffer compartilhado
 tam = 10  # tamanho máximo do buffer
-semaforo = threading.Semaphore()
+semaforo = threading.Semaphore() # Semáforo que controla o acesso ao buffer
 
 
-def inserir_no_buffer(vlr: int):
-    if len(buffer) < tam:
-        buffer.append(vlr)
-    else:
+def inserir_no_buffer(vlr: int): # Simulando um buffer limitado através do tamanho dele
+    if len(buffer) < tam: # Verificando se o tamanho do buffer estrapola o máximo
+        buffer.append(vlr) 
+    else: # Caso estrapole, gera uma excessão
         raise IndexError
 
 
-def produzir_item():
+def produzir_item(): # gera um valor aleatório de 0 até 10
     return random.randint(1, 10)
 
 
-def consumir_item():
+def consumir_item(): #consome o primeiro item da lista
     valor = buffer[0]
     buffer.remove(valor)
     return valor
@@ -34,21 +33,21 @@ def consumir_item():
 
 def produtor():
     while True:
-        if len(buffer) < tam:
-            valor = produzir_item()
-            semaforo.acquire()
-            inserir_no_buffer(valor)
-            print(f"{bcolors.OK}Inserindo {str(valor)} {bcolors.RESET}")
-            semaforo.release()
+        if len(buffer) < tam: # se o buffer ainda não estiver cheio, enche ele
+            valor = produzir_item() # produz um valor
+            semaforo.acquire() # reserva o semáforo do buffer
+            inserir_no_buffer(valor) # insere o valor no buffer
+            print(f"{bcolors.OK}Inserindo {str(valor)} {bcolors.RESET}") # imprime o valor inserido
+            semaforo.release() # solta o semáforo do buffer
 
 
 def consumidor():
     while True:
-        if len(buffer) != 0:
-            semaforo.acquire()
-            valor = consumir_item()
-            print(f"{bcolors.FAIL}Consumindo {str(valor)} {bcolors.RESET}")
-            semaforo.release()
+        if len(buffer) != 0: # se a quantidade de itens armazenada no buffer for diferente de zero
+            semaforo.acquire() # reserva o semáforo do buffer
+            valor = consumir_item() # Consome um item
+            print(f"{bcolors.FAIL}Consumindo {str(valor)} {bcolors.RESET}") #imprime o valor consumido
+            semaforo.release() # solta o semáforo do buffer
 
 
 thread_produtor = threading.Thread(target=produtor)
